b74bbd267beab4fe47ddea648e4b3ac1af1107cf
unknown <ranatanveerhassan04@gmail.com>
2024-03-05 14:05:31 +0500

    ok

---
+++ app.js
@@ -0,0 +1,33 @@
+var jamiastudent = [
+    {
+        id:101, 
+        details:{
+        Name: "zubair Arif",
+        class:"8th",
+        TlimateQuran:55/100,
+        English: 33/100,
+        Math: 45/100,
+        Scince: 12/100,
+        FezainTejwiid: 35/100,
+        TotalMarks:500,
+        ObtainMarks:254,
+        }
+        
+    },
+    {
+        id:101, 
+        details:{
+        Name: "Abu Baker",
+        class:"8th",
+        TlimateQuran:35/100,
+        English: 31/100,
+        Math: 35/100,
+        Scince: 11/100,
+        FezainTejwiid: 25/100,
+        TotalMarks:500,
+        ObtainMarks:194,
+        }
+    },
+
+//   d more objects for additional cities or schools as needed
+];
